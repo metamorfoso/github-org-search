478b9aa941a1d25ac82498a4b72ee92df677adeb
Ivan <ivan@mish.guru>
2020-04-23 11:49:07 +1200

    tweak filter visuals

--- src/Search/Result/FilterResults/index.js
+++ src/Search/Result/FilterResults/index.js
@@ -6,12 +6,12 @@ const FilterResults = ({ onLocationChange, location, onWebsiteChange, website })
   return (
     <div className="filters">
       <div className="filter">
-        <label htmlFor="locationFilter">Location</label>
-        <input name="locationFilter" type="text" value={location} onChange={onLocationChange} />
+        <label className="filterLabel" htmlFor="locationFilter">Location</label>
+        <input className="filterInput" name="locationFilter" type="text" value={location} onChange={onLocationChange} />
       </div>
       <div className="filter">
-        <label htmlFor="websiteFilter">Website</label>
-        <input name="websiteFilter" type="text" value={website} onChange={onWebsiteChange} />
+        <label className="filterLabel" htmlFor="websiteFilter">Website</label>
+        <input className="filterInput" name="websiteFilter" type="text" value={website} onChange={onWebsiteChange} />
       </div>
     </div>
   )
