431f08d1bc83c5b733b67b5b3a1b216dd8b58a3c
Ivan <ivan@mish.guru>
2020-04-26 22:47:06 +1200

    add a link for user to review access on Github if they want to

--- src/Search/SearchBox/index.js
+++ src/Search/SearchBox/index.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react'
 
+import { OpenInNew } from '../../components/ExtrenalLink/OpenInNew'
+import { OpenInNewIcon } from '../../components/ExtrenalLink/OpenInNewIcon'
+
 import './index.css'
+
+const githubAccessReviewUrl = `https://github.com/settings/connections/applications/${process.env.REACT_APP_CLIENT_ID}`
 
 const SearchBox = ({ onSubmit, fetching }) => {
   const onSubmitNoDefault = (query) => (event) => {
@@ -17,10 +22,15 @@ const SearchBox = ({ onSubmit, fetching }) => {
 
   return (
     <div className="searchBox">
-      <form className="searchForm" onSubmit={onSubmitNoDefault(query)}>
-        <input className="searchFormElement searchInput" type="text" onChange={onChange} value={query} />
-        <button disabled={fetching} className="searchFormElement searchSubmit" onClick={onSubmitNoDefault(query)}>Search</button>
-      </form>
+      <div className="searchAlignmentWrapper">
+        <form className="searchForm" onSubmit={onSubmitNoDefault(query)}>
+          <input className="searchFormElement searchInput" type="text" onChange={onChange} value={query} />
+          <button disabled={fetching} className="searchFormElement searchSubmit" onClick={onSubmitNoDefault(query)}>Search</button>
+        </form>
+        <div className="githubAccessReview">
+          <OpenInNew url={githubAccessReviewUrl}>Review this app's privileges on Github<OpenInNewIcon /></OpenInNew>
+        </div>
+      </div>
     </div>
   )
 }
